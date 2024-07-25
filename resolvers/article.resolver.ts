70bfd8fd7d80d1c3feb57968f054ec95a3828dc7
Huynh Van Thanh ( Diner ) <70517648+thanhpro0922@users.noreply.github.com>
2024-07-25 23:32:25 +0700

    1.1 Sắp xếp

--- resolvers/article.resolver.ts
+++ resolvers/article.resolver.ts
@@ -3,10 +3,20 @@ import Category from "../models/category.model";
 
 export const resolversArticle = {
     Query: {
-        getListArticle: async () => {
+        getListArticle: async (_, args) => {
+            const { sortKey, sortValue } = args;
+
+            //@ Sort
+            const sort = {};
+
+            if (sortKey && sortValue) {
+                sort[sortKey] = sortValue;
+            }
+            //@ End Sort
+
             const articles = await Article.find({
                 deleted: false,
-            });
+            }).sort(sort);
 
             return articles;
         },
