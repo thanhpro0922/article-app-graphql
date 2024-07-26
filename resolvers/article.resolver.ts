105bada1f11da1643e886cdfd6348e5fc1dc3216
Huynh Van Thanh ( Diner ) <70517648+thanhpro0922@users.noreply.github.com>
2024-07-26 16:06:37 +0700

    1.4 Tìm kiếm

--- resolvers/article.resolver.ts
+++ resolvers/article.resolver.ts
@@ -11,6 +11,7 @@ export const resolversArticle = {
                 limitItems,
                 filterKey,
                 filterValue,
+                keyword,
             } = args;
 
             const find = {
@@ -33,6 +34,15 @@ export const resolversArticle = {
                 find[filterKey] = filterValue;
             }
             //@ End Filter
+
+            //@ Search
+            if (keyword) {
+                const keywordRegex = new RegExp(keyword, "i");
+                find["title"] = keywordRegex;
+            }
+
+            //@ End Search
+
             const articles = await Article.find(find)
                 .sort(sort)
                 .limit(limitItems)
--- typeDefs/article.typeDefs.ts
+++ typeDefs/article.typeDefs.ts
@@ -17,6 +17,7 @@ export const typeDefsArticle = gql`
             limitItems: Int = 2
             filterKey: String
             filterValue: String
+            keyword: String
         ): [Article]
         getArticle(id: ID): Article
     }
